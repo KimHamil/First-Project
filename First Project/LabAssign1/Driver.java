41366e6d091ff1138a099b6e6f5dcf400cb7c628
KimHamil <55211705+KimHamil@users.noreply.github.com>
2019-09-18 16:33:24 -0500

    Lab Assignment 1

--- First Project/LabAssign1/Driver.java
+++ First Project/LabAssign1/Driver.java
@@ -3,11 +3,38 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	House[] house=new House[3];
+	House[] house1 =new House[3];
 		Scanner input=new Scanner(System.in);
 		for(int i=0;i<3;i++) {
-			Photo photo
+			Photo photo = new Photo();
+			House house1=new House();
+			System.out.println("Enter id: ");
+			house1.setId(input.nextInt());
+
+			System.out.println("Enter price: ");
+			house1.setPrice(input.nextFloat());
+
+			System.out.println("Enter location: ");
+			house1.setLocation(input.nextLine());
+
+			System.out.println("Enter advertiser: ");
+			house1.setAdvertiser(input.nextLine());
+
+			System.out.println("Enter pixel: );
+			photo.setPixel(input.nextFloat());
+
+			System.out.println("Enter byte: );
+			photo.setByte(input.nextFloat());
+
+			input.nextLine();
+			house.setPhoto(photo);
+			house[i] =house1;
+
+		}
+		for(House house1:house){
+			System.out.println(house);
 		}
 	}
+
 
 }
